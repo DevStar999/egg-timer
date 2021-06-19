73f687865656c27c3aaa878057c4db45871d260f
DevStar999 <wolverinerage99@gmail.com>
2021-06-19 17:53:29 +0530

    Setting up the layout and initialising the views

--- app/src/main/java/com/example/eggtimer/MainActivity.java
+++ app/src/main/java/com/example/eggtimer/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.eggtimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private SeekBar eggTimerSeekBar;
+    private TextView eggTimerDisplayTextView;
+    private Button eggTimerTriggerButton;
+
+    private void initialise() {
+        eggTimerSeekBar = findViewById(R.id.eggTimerSeekBar);
+        eggTimerDisplayTextView = findViewById(R.id.eggTimerDisplayTextView);
+        eggTimerTriggerButton = findViewById(R.id.eggTimerTriggerButton);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialise();
     }
 }
